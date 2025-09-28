fa0186c52b4d268f60a8e5c2965b590f6762ade6
Ujjwal <45ujjwal@gmail.com>
2025-09-28 17:36:39 +0530

    getFriendReqs and acceptedReqs endpoint

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -138,3 +138,49 @@ export const acceptFriendRequest = async (req, res) => {
         });
     }
 }
+
+export const getFriendRequests = async (req, res) => {
+    try {
+        const incomingReqs = await FriendRequest.find({
+            recipient: req.user.id,
+            status: "pendind"
+        }).populate("sender", "fullName profilePic nativeLanguage learningLanguage");
+
+        const acceptedReqs = await FriendRequest.find({
+            sender: req.user.id,
+            status: "accepted"
+        }).populate("recipient", "fullName profilePic");
+
+        res.status(400).json({
+            incomingReqs, 
+            acceptedReqs
+        })
+
+    } catch (error) {
+        console.log("Error in getFriendRequests controller ", error.message);
+        res.status(500).json({
+            message: "Internal Server Error"
+        });
+        
+    }
+}
+
+export const getOutgoingFriendReqs = async (req, res) => {
+    try {
+        const outgoingRequests = await FriendRequest.find({
+            sender: req.user.id,
+            status: "pending"
+        }).populate("recipient", "fullName profilePic nativeLanguage learningLanguage");
+
+        res.status(400).json({
+            outgoingRequests
+        });
+
+    } catch (error) {
+        console.log("Error in getOutgoingFriendReqs controller ", error.message);
+        res.status(500).json({
+            message: "Internal Server Error"
+        });
+        
+    }
+}
